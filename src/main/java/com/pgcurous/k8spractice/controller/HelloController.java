53c77ca2032c17b007f62d133db3a3c3da58afd9
Pradipta Gure <pradipta.gure@gmail.com>
2022-07-08 10:30:45 +0530

    Initial Commit. Docker build is successful and I could receive response.

---
+++ src/main/java/com/pgcurous/k8spractice/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.pgcurous.k8spractice.controller;
+
+import com.pgcurous.k8spractice.pojo.Kubernetes;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @Value("${pgcurious.k8s-practice.welcome-message}")
+    private String welcome;
+
+    @GetMapping("/kubernetes")
+    public Kubernetes kubernetes () {
+        log.info("Test started");
+        Kubernetes kubernetes = new Kubernetes();
+        kubernetes.setName("k8s-practice");
+        kubernetes.setCloud("GCP");
+        kubernetes.setType("It is not managed by GCP.");
+        kubernetes.setWelcomeMessage(welcome);
+        return kubernetes;
+    }
+}
---
+++ src/main/java/com/pgcurous/k8spractice/pojo/Kubernetes.java
@@ -0,0 +1,16 @@
+package com.pgcurous.k8spractice.pojo;
+
+import lombok.Data;
+
+@Data
+public class Kubernetes {
+
+    private String name;
+
+    private String cloud;
+
+    private String type;
+
+    private String welcomeMessage;
+
+}
